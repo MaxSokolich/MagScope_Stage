fbffe0702dcf13fbbebe481f6fba237aea32033d
Max Sokolich <50302377+MaxSokolich@users.noreply.github.com>
2023-05-25 17:44:38 -0400

    Update PS4ProcTest.py
    great

--- src/tests/PS4Controller/PS4ProcTest.py
+++ src/tests/PS4Controller/PS4ProcTest.py
@@ -14,10 +14,6 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-     
-
-
-
 
 if __name__ == "__main__":
     #create instance of motor stage class
@@ -38,24 +34,10 @@ if __name__ == "__main__":
     while True:
         try:
             actions = joystick_q.get(0)
-
             if actions == False:
                 break
-        
             else:
-                acoustic = actions[4]
-                
-                
-                if actions[0] == 0:
-                    x = actions[1]
-                    y = actions[2]
-                    z = actions[3]
-                    stage.MoveX(x)
-                    stage.MoveY(y)
-                    stage.MoveZ(z)
-                    print("Stage Moving: ",[x,y,z])
-                else:
-                    arduino.send(actions[0], actions[1], actions[2], actions[3])
+                arduino.send(actions)
         except Empty:
             pass
  
